19cce466e488d11075ebde0b740e57da3a57e2e3
Joel Southall <vextechmage@gmail.com>
2024-10-31 01:01:52 -0700

    Add sparky the dungeon dog!

--- client/src/components/Mascot.js
+++ client/src/components/Mascot.js
@@ -1,13 +1,14 @@
 // src/components/Mascot.js
 import React, { useContext } from 'react';
 import { MascotContext } from '../context/MascotContext';
+import sparkyImage from '../assets/sparky.png'; // Import the image
 
 function Mascot() {
-    const { message, customization } = useContext(MascotContext);
+    const { message } = useContext(MascotContext);
 
     return (
         <div className="mascot">
-            <img src={customization.image} alt="Sparky the Dungeon Dog" />
+            <img src={sparkyImage} alt="Sparky the Dungeon Dog" /> {/* Use the imported image */}
             <p>{message}</p>
         </div>
     );
